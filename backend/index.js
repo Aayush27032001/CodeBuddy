47eed5344a07978bb92a4b98dc140dc8773564fe
n0IQ <varmamohit122@gmail.com>
2023-01-04 15:20:25 +0530

    Added Packages and minor updates

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,58 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const cookieParser = require('cookie-parser');
 const questionRouter = require('./routes/questionRoutes');
+const testRouter = require('./routes/testRoutes');
+const resultRouter = require('./routes/resultRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 const app = express();
 
 // midleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
+// routes
 app.get('/', (req, res) => {
-  res.status(200).send("Hello Server is Up and fine!")
-})
+  res.status(200).send('Hello Server is Up and fine!');
+});
 
-app.post('/js',(req,res)=>{
-    // console.log(req.body);
-    fs.writeFileSync('./JSCodes/Code.js',req.body.code);
-    try{
-      const func = require('./JSCodes/Code');
-      // console.log(func);
-      const testCase = [[1,2,3,4,5],[9,9,55,1,4],[],[1]];
-      const CorrectResults = [15,78,0,1];
-      const userResults = CorrectResults.map((e,i)=>{
-        if(func(testCase[i])===e) return true;
-        return false;
-      })
-      // fs.unlinkSync("./JSCodes/Code.js")
-      res.status(200).json({message:"Evaluation Done!",data:userResults});
-    }
-    catch(e){
-      // fs.unlinkSync("./JSCodes/Code.js")
-      res.status(500).json({message:"Time Limit Exced or Code having syntax error"});
-    }
-})
+app.post('/js', (req, res) => {
+  // console.log(req.body);
+  fs.writeFileSync('./JSCodes/Code.js', req.body.code);
+  try {
+    const func = require('./JSCodes/Code');
+    // console.log(func);
+    const testCase = [[1, 2, 3, 4, 5], [9, 9, 55, 1, 4], [], [1]];
+    const CorrectResults = [15, 78, 0, 1];
+    const userResults = CorrectResults.map((e, i) => {
+      if (func(testCase[i]) === e) return true;
+      return false;
+    });
+    // fs.unlinkSync("./JSCodes/Code.js")
+    res.status(200).json({
+      message: 'Evaluation Done!',
+      data: userResults,
+    });
+  } catch (e) {
+    // fs.unlinkSync("./JSCodes/Code.js")
+    res.status(500).json({
+      message: 'Time Limit Exced or Code having syntax error',
+    });
+  }
+});
 
-// Routes
 app.use('/api/questions', questionRouter);
+app.use('/test', testRouter);
+app.use('/result', resultRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
+dotenv.config({
+  path: './config.env',
+});
 const app = require('./index');
 
 // Connect Database
-
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -18,10 +18,13 @@ mongoose
   })
   .then(() => {
     console.log('Database Successfully Connected');
+  })
+  .catch((err) => {
+    console.log('Database Error');
   });
 
 // Start Server
-const port = 5000;
+const port = 5000 || PORT;
 app.listen(port, () => {
-    console.log(`Server is Up at ${port}`)
+  console.log(`Server running at ${port}`);
 });
