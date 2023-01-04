c36ee3aecb3f9810600d98e2add51e349ea2f41e
n0IQ <varmamohit122@gmail.com>
2023-01-04 15:17:08 +0530

    Created user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,76 @@
+const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    message: 'All users found',
+    data: {
+      users,
+    },
+  });
+});
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'User found',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.createTest = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    message: 'User created successfully',
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+exports.updateTest = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) {
+    return next(new AppError('No User found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'User updated successfully',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.deleteTest = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'User Deleted successfully',
+    data: null,
+  });
+});
