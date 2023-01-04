0e3eab60cb134186ae4ae90297a1d11a72cecb0a
n0IQ <varmamohit122@gmail.com>
2023-01-04 15:17:39 +0530

    Created user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,54 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'Please enter your first name'],
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Please enter your first name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Password do not match',
+    },
+  },
+});
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified(this.password)) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.checkPassword(async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
 });
 
 const User = mongoose.model('User', userSchema);
