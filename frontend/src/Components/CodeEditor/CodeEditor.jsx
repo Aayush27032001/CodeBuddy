a3f3ab0f50f8bf7c956d0e61ea8496e954f80152
Aayush <aayushvishwakarma1999@gmail.com>
2023-04-24 19:34:21 +0530

    codeeditor_changes

--- frontend/src/Components/CodeEditor/CodeEditor.jsx
+++ frontend/src/Components/CodeEditor/CodeEditor.jsx
@@ -6,50 +6,45 @@ import { lintGutter, linter } from "@codemirror/lint";
 import Linter from "eslint4b-prebuilt";
 import { Box, Button, Typography } from "@mui/material";
 import { toast } from "react-toastify";
+
 const esLintConfigs = {
   env: { es6: true },
   rules: { "no-unused-vars": "off" },
 };
+
 const CodeEditor = ({ question }) => {
-  const [code, setCode] = useState(
-    "module.exports = function(input) {\n  //Your code goes here\n\n}"
+  const [code, setCode] = useState(() =>
+    localStorage.getItem(question?._id) ||
+      "module.exports = function(input) {\n  //Your code goes here\n\n}"
   );
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
+
   const submitHandler = async () => {
     setLoading(true);
-    const res = await post("js", {
-      code,
-      testId: localStorage.getItem("testCode"),
-      questionId: question?._id,
-    });
-    if (!res) {
-      toast("Your Code Harmed our server there may be infinte loops!",{type:"error", position:"top-right"});
-      setLoading(false);
-      setResults([]);
-    }
-    if (res.ok) {
+    try {
+      const res = await post("js", {
+        code,
+        testId: localStorage.getItem("testCode"),
+        questionId: question?._id,
+      });
+      if (!res.ok) {
+        throw new Error(res.message);
+      }
       setResults(res.data);
+    } catch (error) {
+      toast(error.message, { type: "error", position: "top-right" });
+    } finally {
       setLoading(false);
-    } else {
-      setLoading(false);
-      setResults([]);
-      toast(res.message,{type:"error", position:"top-right"});
     }
   };
 
-  useEffect(() => {
-    if (localStorage.getItem(question?._id)) {
-      setCode(localStorage.getItem(question?._id));
-    }
-    setResults([]);
-  }, [question?._id]);
   useEffect(() => {
     localStorage.setItem(question?._id, code);
   }, [code, question?._id]);
 
   return (
-    <div style={{ position: "relative", height: "100%" }}>
+    <Box height="100%" position="relative">
       <ReactCodeMirror
         value={code}
         theme="light"
@@ -59,44 +54,46 @@ const CodeEditor = ({ question }) => {
           linter(esLint(new Linter(), esLintConfigs)),
           lintGutter(),
         ]}
-        onChange={(val) => {
-          setCode(val);
-        }}
+        onChange={(value) => setCode(value)}
       />
-      <Box sx={{height:"30vh", overflow:"auto"}}>
+      <Box sx={{ height: "30vh", overflow: "auto" }}>
         {!loading &&
-          results?.map((e, i) => {
-            return (
-              <Typography key={i} sx={{m:"0.25rem", background: e ? "rgba(221,255,221,1.00)" : "#f9c9c9", p: "1rem", borderRadius:"8px"}}>
-                {`Test Case ${i + 1}: `}
-                {e === true ? "Passed" : "Failed"}
-              </Typography>
-            );
-          })}
+          results.map((result, index) => (
+            <Typography
+              key={index}
+              sx={{
+                m: "0.25rem",
+                background: result ? "rgba(221,255,221,1.00)" : "#f9c9c9",
+                p: "1rem",
+                borderRadius: "8px",
+              }}
+            >
+              {`Test Case ${index + 1}: `}
+              {result ? "Passed" : "Failed"}
+            </Typography>
+          ))}
       </Box>
-      <Box>
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: 0,
-            width: "100%",
-            background: "#f5f5f5",
-            display: "flex",
-            height: "4vh",
-            justifyContent: "flex-end",
-          }}
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          background: "#f5f5f5",
+          display: "flex",
+          height: "4vh",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button
+          onClick={submitHandler}
+          disabled={loading}
+          variant="contained"
+          color="error"
         >
-          <Button
-            onClick={submitHandler}
-            disabled={loading}
-            variant="contained"
-            color="error"
-          >
-            Run Test
-          </Button>
-        </Box>
+          Run Test
+        </Button>
       </Box>
-    </div>
+    </Box>
   );
 };
 
